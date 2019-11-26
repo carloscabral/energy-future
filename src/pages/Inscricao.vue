6b390f3565d4554e1d40988bd22f0ee5bb4f15ae
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2019-11-25 23:56:47 -0300

    Minimum text change

--- src/pages/Inscricao.vue
+++ src/pages/Inscricao.vue
@@ -44,7 +44,7 @@
                     <h3 class="card__number">03.</h3>
                     <h4 class="card__title">Preencha o relatório de apresentação</h4>
                     <p class="card__divider card__divider--3"></p>
-                    <p class="card__text">Ao preencher o formulário de inscrição, na mesma plataforma, você deve fazer o envio do “relatório de detalhamento” do seu projeto.  É muito importante para validar a sua participação, que você o preencha e faça o upload do arquivo no formulário.</p>
+                    <p class="card__text">Ao preencher o formulário de inscrição, na mesma plataforma, você deve fazer o envio do “relatório de apresentação do seu projeto.  É muito importante para validar a sua participação, que você o preencha e faça o upload do arquivo no formulário.</p>
                     <g-link to="../assets/images/Relatorio-de-Apresentacao.pptx" class="card__link card__link--3"><font-awesome :icon="['fas', 'arrow-right']"/>Relatório de apresentação</g-link>
                 </div>           
               </div>
