b27f90813c99d03c63e47e896d5f8f9e172d1d1e
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2020-01-23 14:48:03 -0300

    Fixed the height of mobile menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -147,7 +147,7 @@ export default {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -65%);
     list-style: none;
     text-align: center;
     padding: 0;
