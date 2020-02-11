56a9c67c123ce5b66b5c2c8fece61bc08c707d74
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2020-02-10 21:53:47 -0300

    Added a new Landing Page as a second marketing option

--- src/pages/Inscricao-lp.vue
+++ src/pages/Inscricao-lp.vue
@@ -1,14 +1,17 @@
 <template>
-  <Newest>   
+  <Newest> 
+      <div class="row my-4">
+          <g-image alt="Logo Energy Future" src="@/assets/images/img-logo-ef.svg" />
+      </div>
+      <div class="lp-hero__bg"></div>  
       <div class="row mt-5">
           <div class="col-xl-6 hero">
               <h1 class="lp-hero__title">A maior chamada de projetos de P&D do setor elétrico brasileiro.</h1>
               <h3 class="lp-hero__subtitle">O Energy Future, hub virtual de inovação do setor elétrico, conecta projetos a grandes concessionárias do país. Procuramos soluções inovadoras que ajudem a melhorar a geração, distribuição e transmissão de energia elétrica no Brasil.</h3>
-              <button class="lp-hero__button">Faça seu cadastro</button>
+              <g-link to="https://energyfuture.typeform.com/to/QvbsS6" target="_blank"><button class="lp-hero__button">Faça seu cadastro</button></g-link>
           </div>
           <div class="col-xl-6 mt-4 mt-xl-0 lp-hero__video">
               <iframe title="Video de destaque" src="https://player.vimeo.com/video/389214543" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-              <!-- <iframe title="Video de destaque" src="https://www.youtube.com/embed/bamqYn5JXq4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
           </div>
       </div>
       <div class="row">
@@ -17,12 +20,12 @@
           </div>
           <div class="col-12 companies">
               <ul class="companies__list">
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-aes.svg" with="100" /></li>
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-energisa.svg" with="100" /></li>
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-equatorial.png" with="100" /></li>
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-enel.svg" with="100" /></li>
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-light.svg" with="100" /></li>
-                  <li class="companies__item"><g-image src="../assets/images/img-logo-antonio.svg" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo AES" src="../assets/images/img-logo-aes.svg" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo Energisa" src="../assets/images/img-logo-energisa.svg" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo Equatorial" src="../assets/images/img-logo-equatorial.png" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo Enel" src="../assets/images/img-logo-enel.svg" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo Light" src="../assets/images/img-logo-light.svg" with="100" /></li>
+                  <li class="companies__item"><g-image alt="Logo Sto. Antonio" src="../assets/images/img-logo-antonio.svg" with="100" /></li>
               </ul>
           </div>
       </div>
@@ -32,23 +35,23 @@
                 <h3 class="color-box__title">Quem pode se inscrever no Energy Future?</h3>
             </div>
             <div class="col-lg-2 col-md-4 col-sm-6">
-                <g-image class="color-box__icon" src="../assets/images/new-page/ic-startups.svg" width="100" />
+                <g-image alt="Logo Startups" class="color-box__icon" src="../assets/images/new-page/ic-startups.svg" width="100" />
                 <p class="color-box__text">Startups</p>
             </div>
             <div class="col-lg-2 col-md-4 col-sm-6">
-                <g-image class="color-box__icon" src="../assets/images/new-page/ic-pesquisadores.svg" width="100" />
+                <g-image alt="Logo Pesquisadores" class="color-box__icon" src="../assets/images/new-page/ic-pesquisadores.svg" width="100" />
                 <p class="color-box__text">Pesquisarores</p>
             </div>
             <div class="col-lg-2 col-md-4 col-sm-6">
-                <g-image class="color-box__icon" src="../assets/images/new-page/ic-corporacoes.svg" width="100" />
+                <g-image alt="Logo Corporações" class="color-box__icon" src="../assets/images/new-page/ic-corporacoes.svg" width="100" />
                 <p class="color-box__text">Corporações</p>
             </div>
             <div class="col-lg-2 col-md-4 col-sm-6">
-                <g-image class="color-box__icon" src="../assets/images/new-page/ic-pequenas-empresas.svg" width="100" />
+                <g-image alt="Logo Pequenas Empresas" class="color-box__icon" src="../assets/images/new-page/ic-pequenas-empresas.svg" width="100" />
                 <p class="color-box__text">Pequenas Empresas</p>
             </div>
             <div class="col-lg-2 col-md-4 col-sm-6">
-                <g-image class="color-box__icon" src="../assets/images/new-page/ic-medias-empresas.svg" width="100" />
+                <g-image alt="Logo Médias Empresas" class="color-box__icon" src="../assets/images/new-page/ic-medias-empresas.svg" width="100" />
                 <p class="color-box__text">Médias Empresas</p>
             </div>                        
         </div>
@@ -83,16 +86,16 @@
           <div class="row">
               <div class="col-xl-4 col-lg-6 col-md-6 mt-5 px-xl-2">
                   <div class="new-card new-card--1">
-                      <h3 class="new-card__icon"><g-image class="color-box__icon" src="../assets/images/new-page/img-regulamentos.svg" width="75" /></h3>
+                      <h3 class="new-card__icon"><g-image alt="Ícone de regulamento" class="color-box__icon" src="../assets/images/new-page/img-regulamentos.svg" width="75" /></h3>
                       <h4 class="new-card__title">O que preciso saber para a inscrição no Energy Future?</h4>
                       <p class="new-card__divider new-card__divider--1"></p>
                       <g-link to="../assets/images/Regulamento-Energy-Future-Oficial.pdf" target="_blank" class="new-card__link new-card__link--1"><font-awesome :icon="['fas', 'arrow-right']"/>Regulamento Energy Future</g-link>
-                      <g-link to="../assets/images/Manual-PDANEEL.pdf" target="_blank" class="new-card__link new-card__link--1"><font-awesome :icon="['fas', 'arrow-right']"/>Regulamento Aneel</g-link>
+                      <g-link to="../assets/images/PROP&D.zip" target="_blank" class="new-card__link new-card__link--1"><font-awesome :icon="['fas', 'arrow-right']"/>Regulamento Aneel</g-link>
                   </div>
               </div>
               <div class="col-xl-4 col-lg-6 col-md-6 mt-5 px-xl-2">
                 <div class="new-card new-card--2">
-                    <h3 class="new-card__icon"><g-image class="color-box__icon" src="../assets/images/new-page/img-encerramento.svg" width="75" /></h3>
+                    <h3 class="new-card__icon"><g-image alt="Ícone de prazo de inscrição" class="color-box__icon" src="../assets/images/new-page/img-encerramento.svg" width="75" /></h3>
                     <h4 class="new-card__title">Até quando posso realizar a inscrição?</h4>
                     <p class="new-card__divider new-card__divider--2"></p>
                     <g-link to="/inscricao/" target="_blank" class="new-card__link new-card__link--2">O encerramento das inscrições será no dia <strong>30/01/2020</strong></g-link>
@@ -100,10 +103,10 @@
               </div>
               <div class="col-xl-4 col-lg-6 col-md-6 mt-5 px-xl-2">
                 <div class="new-card new-card--3">
-                    <h3 class="new-card__icon"><g-image class="color-box__icon" src="../assets/images/new-page/img-faq.svg" width="75" /></h3>
+                    <h3 class="new-card__icon"><g-image alt="Ícone de dúvidas" class="color-box__icon" src="../assets/images/new-page/img-faq.svg" width="75" /></h3>
                     <h4 class="new-card__title">Dúvidas? Veja nossas perguntas e respostas mais frequentes:</h4>
                     <p class="new-card__divider new-card__divider--3"></p>
-                    <g-link to="../assets/images/Relatorio-de-Apresentacao.pptx" class="new-card__link new-card__link--3"><font-awesome :icon="['fas', 'arrow-right']"/>Acesse nosso FAQ</g-link>
+                    <g-link to="/duvidas/" class="new-card__link new-card__link--3"><font-awesome :icon="['fas', 'arrow-right']"/>Acesse nosso FAQ</g-link>
                 </div>           
               </div>
           </div>
@@ -112,47 +115,24 @@
       <div class="call-to-action">
           <div class="row text-center">
               <div class="col-12">
-                  <button class="cta-btn">Faça seu cadastro</button>
+                  <g-link to="https://energyfuture.typeform.com/to/QvbsS6" target="_blank"><button class="cta-btn">Faça seu cadastro</button></g-link>
               </div>
           </div>
       </div>
 
       <div class="lp-footer">
           <div class="row">
-              <div class="col-sm-2 col-md-1">Apoio</div>
-              <div class="col-sm-10 col-md-11 lp-footer-line"></div>
+              <h3 class="col-12 text-center mb-4">Apoio</h3>
           </div>
           <div class="row text-center">
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
+              <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2" v-for="(logo, index) in logoNames" :key="index">
+                  <g-image class="lp-supporters" alt="Logo da empresa apoiadora " :src="require(`../assets/images/${logo}`)" width="75"></g-image>
               </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
-              <div class="col-6 col-sm-4 col-md-2">
-                  <g-image class="lp-bottom-logos" src="../assets/images/img-logo-apoio-centralcomm.svg" width="75"></g-image>
-              </div>
+          </div>
+          <div class="row my-5 align-items-end">
+              <div class="my-3 my-lg-0 text-center text-md-left col-lg-3"><g-image alt="Logo Energy Future" class="lp-footer__bottom-logos ef" src="../assets/images/img-logo-ef-light.svg" width="200" /></div>
+              <div class="my-3 my-lg-0 text-center text-md-left col-lg-3"><g-image alt="Logo PD Aneel" class="lp-footer__bottom-logos aneel" src="../assets/images/img-logo-pdaneel-light.svg" width="200" /></div>
+              <g-link to="/inscricao/" class="my-3 my-lg-0 text-center text-md-left text-lg-right col-lg-6 lp-footer-link">Inscreva seu projeto&nbsp;&nbsp;<font-awesome :icon="['fas', 'arrow-right']"/></g-link>
           </div>
       </div>
 
@@ -163,6 +143,17 @@
 import Collapse from '~/components/Collapse.vue'
 
 export default {
+  metaInfo: {
+    title: 'Inscrição Energy Future',
+    meta: [
+      { charset: 'utf-8' },
+      { name: "author", content: "Carlos Gomes Cabral" },
+      { name: "description", content: "O setor elétrico está buscando projetos e startups que possam trazer inovação na geração, transmissão ou distribuição de energia. Inscreva-se!" },
+      { name: "keywords", content: "Startups, Inovação, Setor elétrico, Concessionárias de energia" },
+      { key: 'og:title', name: 'og:title', content: 'Energy Future - Inscreva-se' },
+      { key: 'twitter:title', name: 'twitter:title', content: 'Energy Future - Inscreva-se' }      
+    ]
+  },    
   data: () => ({
     activeTestimonial: 0,
     items : [
@@ -172,6 +163,34 @@ export default {
         { title : 'Novos Negócios em Geração e Transmissão', text: 'Desenvolvimento de soluções inovadoras ou novas formas de geração e transmissão voltadas para B2G, B2B e B2C.' },
         { title : 'Serviços ao Cliente de Distribuição', text: 'Novos serviços e soluções direcionados para o cliente consumidor de energia elétrica; hardware e Software aplicados à mobilidade elétrica; novas formas de arrecadação das contas de energia: serviços ﬁnanceiros e soluções para clientes não bancarizados.' },
         { title : 'Performance e Digitalização', text: 'Inovação e eﬁciência na geração de produtos e serviços que melhorem o dia a dia do setor de energia; soluções em tecnologia e processos, por meio da conectividade e da troca e envio de dados.' }
+    ],
+    logoNames: [
+        "img-apoio-cesar.svg",
+        "img-apoio-open-innovation.svg",
+        "img-apoio-abmen.svg",
+        "img-apoio-abraceel.svg",
+        "img-apoio-firjan.svg",
+        "img-apoio-parque.svg",
+        "img-apoio-coppe.svg",
+        "img-apoio-startupi.svg",
+        "img-apoio-fiec.svg",
+        "img-apoio-senai.svg",
+        "img-apoio-senai4.svg",
+        "img-apoio-midas.svg",
+        "img-apoio-abradee.svg",
+        "img-apoio-anjos.svg",
+        "img-apoio-fiep.svg",
+        "img-apoio-fiems.svg",
+        "img-apoio-senai2.svg",
+        "img-apoio-fits.svg",
+        "img-apoio-fiepa.svg",
+        "img-apoio-fiero.svg",
+        "img-apoio-cni.svg",
+        "img-apoio-estrategico-abacomm.svg",
+        "img-apoio-estrategico-centralcomm.svg",
+        "img-apoio-estrategico-acate.svg",
+        "img-apoio-estrategico-aqwa.svg",
+        "img-apoio-estrategico-ist.svg",        
     ],
     testimonials: [
       { author : 'Sunny Jonathan - Coordenador de P&D&I no IST ENERGIA/SENAI SP', text: '“O IST ENERGIA do SENAI São Paulo apoia o ENERGY FUTURE, pois é uma grande oportunidade para viabilizar projetos com produtos e resultados que agreguem valor aos negócios das concessionárias do setor elétrico, principalmente nas fases finais da cadeia de inovação, onde o IST ENERGIA tem seu foco de atuação.”' },
@@ -195,6 +214,31 @@ export default {
 <style lang="scss">
 
 .lp-hero {
+
+    &__bg {
+        @media(min-width: 992px) {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: url("../assets/images/img-graphism-bg.svg");
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position-y: 2%;
+            opacity: .35;
+            z-index: -999;   
+        }
+
+        @media(min-width: 1500px) {
+            transform: translateY(6rem);
+        }
+
+        @media(min-width: 2050px) {
+            display: none;
+        }         
+    }
+
     &__title {
         color: $primary-color !important;
     }
@@ -236,8 +280,8 @@ export default {
 
 
 .line-title {
-    margin: 6rem 0 0;
-    color: rgba($primary-color, .75) !important;
+    margin: 6rem 0 1.5rem;
+    color: $primary-color !important;
     @include news-headline($accent-color-1);
 
     @media (min-width: 768px) {
@@ -389,7 +433,8 @@ export default {
     border-radius: .55rem;
     overflow: hidden;
     max-width: 22rem;
-    transition: box-shadow .2s ease;   
+    transition: box-shadow .2s ease;
+    min-height: 18.44rem;   
 
     &:hover {
         box-shadow: 0 3rem 5rem -1rem rgba($primary-color, .16);
@@ -518,11 +563,34 @@ export default {
 
 .lp-footer {
     margin-top: 6rem;
+    position: relative;    
+}
+
+.lp-supporters {
+    //margin: 1rem;
 }
 
 .lp-bottom-logos {
-    filter: grayscale(100%);
-    margin: 1rem;
+    width: 100%;
+}
+
+.lp-footer-link {
+    text-transform: uppercase;
+    color: #fff !important;
+    font-size: .88rem;
+    font-weight: 500;
+    letter-spacing: .7px;
+
+    svg {
+        color: $accent-color-2;
+    }
+
+}
+
+.aneel {
+    @media (min-width: 768px) {
+        transform: translateY(5px);
+    }
 }
 
 .lp-footer-line {
